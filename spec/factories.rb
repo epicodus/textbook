ee8a757fbcbac175c12bedf251d7498563d29b89
michael <michaelrkn@gmail.com>
2012-11-08 16:59:30 -0800

    enclose faker generated data in curly braces to prevent duplication

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,23 +1,23 @@
 FactoryGirl.define do
   factory :author, :class => User do
-    email Faker::Internet.email
-    password Faker::Lorem.words(3).join
+    email {Faker::Internet.email}
+    password {Faker::Lorem.words(3).join}
     author true
   end
 
   factory :student, :class => User do
-    email Faker::Internet.email
-    password Faker::Lorem.words(3).join
+    email {Faker::Internet.email}
+    password {Faker::Lorem.words(3).join}
   end
 
   factory :section do
-    name Faker::Lorem.words(2).join
+    name {Faker::Lorem.words(2).join}
     sequence(:sort_order) {|n| n}
   end
 
   factory :page do
-    title Faker::Lorem.words(3).join(" ")
-    body Faker::Lorem.paragraph
+    title {Faker::Lorem.words(3).join(" ")}
+    body {Faker::Lorem.paragraph}
     association :section, :factory => :section
     sequence(:sort_order) {|n| n}
   end
