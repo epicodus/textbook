3bcf8a39a78b938b43f23f39f469930ab758b4f8
michael <michaelrkn@gmail.com>
2012-11-12 13:10:05 -0800

    fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -40,12 +40,6 @@ ActiveRecord::Schema.define(:version => 20121112161924) do
     t.integer  "chapter_id", :null => false
   end
 
-  create_table "sites", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
     t.string   "encrypted_password",     :default => "",    :null => false
