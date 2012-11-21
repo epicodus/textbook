b6d9fe0134ca72a0453951d6aa9d301e0e4a83a0
michael <michaelrkn@gmail.com>
2012-11-21 15:42:26 -0800

    fix broken migration

--- db/migrate/20121121230440_add_unique_index_to_lesson_section_chapter_names.rb
+++
@@ -1,7 +0,0 @@
-class AddUniqueIndexToLessonSectionChapterNames < ActiveRecord::Migration
-  def change
-    add_index :lessons, :name, :unique => :true
-    add_index :sections, :name, :unique => :true
-    add_index :chapters, :name, :unique => :true
-  end
-end
--- db/migrate/20121121231648_add_unique_index_to_lesson_section_and_chapter_name.rb
+++ db/migrate/20121121231648_add_unique_index_to_lesson_section_and_chapter_name.rb
@@ -1,4 +1,7 @@
 class AddUniqueIndexToLessonSectionAndChapterName < ActiveRecord::Migration
   def change
+    add_index :lessons, :name, :unique => :true
+    add_index :sections, :name, :unique => :true
+    add_index :chapters, :name, :unique => :true
   end
 end
