dd67be392428fed8e61016b5e30f59edacfc8fa3
michael <michaelrkn@gmail.com>
2012-11-27 13:02:11 -0800

    clean up factories and avoid names that confuse capybara

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,28 +1,28 @@
 FactoryGirl.define do
   factory :author, :class => User do
     email {Faker::Internet.email}
-    password {Faker::Lorem.words(3).join}
+    password {Faker::Lorem.words.join}
     author true
   end
 
   factory :student, :class => User do
     email {Faker::Internet.email}
-    password {Faker::Lorem.words(3).join}
+    password {Faker::Lorem.words.join}
   end
 
   factory :chapter do
-    name {Faker::Lorem.words(1).join}
+    name {Faker::Lorem.words.join}
     sequence(:number) {|n| n}
   end
 
   factory :section do
-    name {Faker::Lorem.words(2).join}
+    name {Faker::Lorem.words.join}
     association :chapter, :factory => :chapter
     sequence(:number) {|n| n}
   end
 
   factory :lesson do
-    name {Faker::Lorem.words(3).join(" ")}
+    name {Faker::Lorem.words.join}
     content {Faker::Lorem.paragraph}
     association :section, :factory => :section
     sequence(:number) {|n| n}
