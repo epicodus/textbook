69e5d7895a75e7687e359e4ceb616003e8ac4679
michael <michaelrkn@gmail.com>
2012-11-28 15:57:59 -0800

    changed migration to not make all lessons public

--- db/migrate/20121127204818_add_public_to_lesson.rb
+++ db/migrate/20121127204818_add_public_to_lesson.rb
@@ -1,6 +1,5 @@
 class AddPublicToLesson < ActiveRecord::Migration
   def change
     add_column :lessons, :public, :boolean, :default => false, :null => false
-    Lesson.all.each {|lesson| lesson.update_attributes(:public => true)}
   end
 end
