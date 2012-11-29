264eadbd8b70a49387f9597c47bae907405494f6
michael <michaelrkn@gmail.com>
2012-11-28 16:40:01 -0800

    fix home controller redirect to nil if no page is public

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def show
-    if Lesson.any?
+    if Lesson.accessible_by(current_ability).any?
       redirect_to Lesson.accessible_by(current_ability).first
     end
   end
