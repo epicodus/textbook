8120eb2eb42c0b4a9d5a5f0574289847febefe2b
michael <michaelrkn@gmail.com>
2013-03-01 16:00:17 -0800

    move to cdn-hosted prettify.js

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ require "application_responder"
 
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
-  respond_to :html
+  respond_to :html, :js
 
   protect_from_forgery
 
