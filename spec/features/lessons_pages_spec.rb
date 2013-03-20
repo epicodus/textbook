c9e6ff70f7a9f251fb3c25ff0ca142290f7b7af9
michael <michaelrkn@gmail.com>
2013-03-20 15:41:53 -0700

    organize specs better

--- spec/features/lessons_pages_spec.rb
+++ spec/features/lessons_pages_spec.rb
@@ -1,37 +1,41 @@
 require 'spec_helper'
 
 describe Lesson do
-  it 'can be created by an author' do
-    create_author_and_sign_in
-    visit chapters_path
-    page.should have_content 'New lesson'
-    visit new_lesson_path
-    page.should have_content 'New lesson'
+  context 'creating' do
+    it 'can be created by an author' do
+      create_author_and_sign_in
+      visit chapters_path
+      page.should have_content 'New lesson'
+      visit new_lesson_path
+      page.should have_content 'New lesson'
+    end
+
+    it 'cannot be created by a student' do
+      create_student_and_sign_in
+      visit chapters_path
+      page.should_not have_content 'New lesson'
+      visit new_lesson_path
+      page.should_not have_content 'New lesson'
+    end
   end
 
-  it 'cannot be created by a student' do
-    create_student_and_sign_in
-    visit chapters_path
-    page.should_not have_content 'New lesson'
-    visit new_lesson_path
-    page.should_not have_content 'New lesson'
-  end
+  context 'editing' do
+    it 'can be edited by an author' do
+      create_author_and_sign_in
+      lesson = FactoryGirl.create :lesson
+      visit lesson_path lesson
+      click_link "Edit #{lesson.name}"
+      page.should have_content 'Edit'
+    end
 
-  it 'can be edited by an author' do
-    create_author_and_sign_in
-    lesson = FactoryGirl.create :lesson
-    visit lesson_path lesson
-    click_link "Edit #{lesson.name}"
-    page.should have_content 'Edit'
-  end
-
-  it 'cannot be edited by a student' do
-    create_student_and_sign_in
-    lesson = FactoryGirl.create :lesson
-    visit lesson_path lesson
-    page.should_not have_content 'Edit lesson'
-    visit edit_lesson_path lesson
-    page.should_not have_content 'Edit'
+    it 'cannot be edited by a student' do
+      create_student_and_sign_in
+      lesson = FactoryGirl.create :lesson
+      visit lesson_path lesson
+      page.should_not have_content 'Edit lesson'
+      visit edit_lesson_path lesson
+      page.should_not have_content 'Edit'
+    end
   end
 
   context 'when it is not public' do
