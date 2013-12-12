021c4b599815126286c32be8edb35b904df7fced
michael <michaelrkn@gmail.com>
2013-12-12 09:46:55 -0800

    raise default capybara wait time to 30

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ Spork.prefork do
 
   Capybara.default_driver = :rack_test
   Capybara.javascript_driver = :poltergeist
-  Capybara.default_wait_time = 10
+  Capybara.default_wait_time = 30
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
