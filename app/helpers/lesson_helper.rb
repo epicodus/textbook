289b410788743236fb7b319d43b7647420baca56
michael <michaelrkn@gmail.com>
2013-12-23 11:38:52 -0800

    fix markdown prettification

--- app/helpers/lesson_helper.rb
+++ app/helpers/lesson_helper.rb
@@ -1,10 +1,8 @@
 module LessonHelper
   def markdown(text)
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-                            :autolink => true,
-                            :space_after_headers => true,
-                            :fenced_code_blocks => true,
-                            :prettify => true)
-    .render(text).html_safe
+    html = Redcarpet::Render::HTML.new(:prettify => true)
+    markdown = Redcarpet::Markdown.new(html, :autolink => true, :space_after_headers => true,
+                                             :fenced_code_blocks => true, :prettify => true)
+    markdown.render(text).html_safe
   end
 end
