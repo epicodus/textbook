b5fd199fa4cae97c1e5dd6c5c24ea5dabcb9098a
michael <michaelrkn@gmail.com>
2013-12-23 12:55:50 -0800

    remove prettyprinting

--- app/helpers/lesson_helper.rb
+++ app/helpers/lesson_helper.rb
@@ -1,6 +1,6 @@
 module LessonHelper
   def markdown(text)
-    html = Redcarpet::Render::HTML.new(:prettify => true)
+    html = Redcarpet::Render::HTML.new
     markdown = Redcarpet::Markdown.new(html, :autolink => true, :space_after_headers => true,
                                              :fenced_code_blocks => true)
     markdown.render(text).html_safe
