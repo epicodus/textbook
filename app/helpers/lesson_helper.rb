e29cdfeff7d9e591a99ec87ede3e86f29374cfda
michael <michaelrkn@gmail.com>
2013-12-23 17:55:05 -0800

    remove autolinking from markdown

--- app/helpers/lesson_helper.rb
+++ app/helpers/lesson_helper.rb
@@ -1,7 +1,7 @@
 module LessonHelper
   def markdown(text)
     html = Redcarpet::Render::HTML.new
-    markdown = Redcarpet::Markdown.new(html, :autolink => true, :space_after_headers => true,
+    markdown = Redcarpet::Markdown.new(html, :space_after_headers => true,
                                              :fenced_code_blocks => true)
     markdown.render(text).html_safe
   end
