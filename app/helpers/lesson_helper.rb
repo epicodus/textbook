42e5bad73ea17e57283c47c37f10ec4ca86618cc
michael <michaelrkn@gmail.com>
2013-12-26 14:55:02 -0800

    add prettyprint to code blocks

--- app/helpers/lesson_helper.rb
+++ app/helpers/lesson_helper.rb
@@ -1,6 +1,6 @@
 module LessonHelper
   def markdown(text)
-    html = Redcarpet::Render::HTML.new
+    html = Redcarpet::Render::HTML.new(:prettify => true)
     markdown = Redcarpet::Markdown.new(html, :space_after_headers => true,
                                              :fenced_code_blocks => true)
     markdown.render(text).html_safe
