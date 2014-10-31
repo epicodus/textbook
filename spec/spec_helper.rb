c5232542a1482529dbd3e2f261a1980d8eeb3376
michael <michaelrkn@gmail.com>
2014-10-31 12:59:49 -0700

    clean up spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,47 +1,28 @@
-require 'rubygems'
-
 ENV["RAILS_ENV"] ||= 'test'
+
+require 'rubygems'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/poltergeist'
 
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
 Capybara.default_driver = :rack_test
 Capybara.javascript_driver = :poltergeist
+
+ActiveRecord::ConnectionAdapters::ConnectionPool.class_eval do
+  def current_connection_id
+    Thread.main.object_id
+  end
+end
 
 include Warden::Test::Helpers
 Warden.test_mode!
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
 RSpec.configure do |config|
-  # Make it so poltergeist (out of thread) tests can work with transactional fixtures
-  # REF http://opinionated-programmer.com/2011/02/capybara-and-selenium-with-rspec-and-rails-3/#comment-220
-  ActiveRecord::ConnectionAdapters::ConnectionPool.class_eval do
-    def current_connection_id
-      Thread.main.object_id
-    end
-  end
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
   config.use_transactional_fixtures = true
-
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
   config.order = "random"
   config.infer_spec_type_from_file_location!
 end
