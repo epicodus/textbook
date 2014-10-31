64ace4945b0eda190279e493f6248351a75d9de3
michael <michaelrkn@gmail.com>
2014-10-31 12:51:39 -0700

    add test for signing in

---
+++ spec/features/devise_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'logging in' do
+  it 'works' do
+    student = FactoryGirl.create :student
+    visit new_user_session_path
+    fill_in 'Email', with: student.email
+    fill_in 'Password', with: student.password
+    click_button 'Sign in'
+    page.should have_content "Signed in successfully."
+  end
+end
