0decb2918de1a4584d9b1e0e4ad5e56afbf8bec3
Christopher Joy-Webb <chris@joywebb.com>
2015-10-16 11:50:40 -0700

    remove Lesson new action from nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Textbook::Application.routes.draw do
   get 'table_of_contents' => redirect('table-of-contents')
 
   resources :sections do
-    resources :lessons
+    resources :lessons, except: [:new]
   end
+  resources :lessons, only: [:new]
   resources :chapters do
     collection do
       put :update_multiple, :path => ''
