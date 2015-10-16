bacd693cd17a55599be5a9ad51b30c485bcaee85
Christopher Joy-Webb <chris@joywebb.com>
2015-10-16 12:14:00 -0700

    update LessonsController show action to include @lesson and @section

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -20,6 +20,11 @@ class LessonsController < InheritedResources::Base
     end
   end
 
+  def show
+    @lesson = Lesson.find(params[:id])
+    @section = Section.find(params[:section_id])
+  end
+
 private
 
   def permitted_params
