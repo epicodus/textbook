89d86880735c7a83701f48cbd1f9e6b17dbfcc34
Christopher Joy-Webb <chris@joywebb.com>
2015-10-27 10:04:35 -0700

    update seed migration to not add deleted lessons to join table

--- db/migrate/20151015231120_add_lesson_id_and_section_id_and_number_to_lesson_sections.rb
+++ db/migrate/20151015231120_add_lesson_id_and_section_id_and_number_to_lesson_sections.rb
@@ -1,7 +1,9 @@
 class AddLessonIdAndSectionIdAndNumberToLessonSections < ActiveRecord::Migration
   def up
-    Lesson.find_by_sql("SELECT section_id, id, number, created_at, updated_at FROM lessons").each do |lesson|
-      ActiveRecord::Base.connection.execute "INSERT INTO lesson_sections (section_id, lesson_id, number, created_at, updated_at) VALUES (#{lesson.section_id}, #{lesson.id}, #{lesson.number}, '#{Time.zone.now.to_formatted_s(:db)}', '#{Time.zone.now.to_formatted_s(:db)}')"
+    Lesson.find_by_sql("SELECT section_id, id, number, deleted_at, created_at, updated_at FROM lessons").each do |lesson|
+      if lesson.deleted_at == nil
+        ActiveRecord::Base.connection.execute "INSERT INTO lesson_sections (section_id, lesson_id, number, created_at, updated_at) VALUES (#{lesson.section_id}, #{lesson.id}, #{lesson.read_attribute(:number)}, '#{Time.zone.now.to_formatted_s(:db)}', '#{Time.zone.now.to_formatted_s(:db)}')"
+      end
     end
   end
 
