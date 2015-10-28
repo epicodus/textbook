08cc78747a624ec8e8825391c937ce7926db796a
Christopher Joy-Webb <chris@joywebb.com>
2015-10-28 11:48:29 -0700

    update ChaptersController update_multiple action to work with lessons_sections join table

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -3,6 +3,13 @@ class ChaptersController < InheritedResources::Base
 
   def update_multiple
     Chapter.update(params[:chapters].keys, params[:chapters].values)
+    sections = params[:chapters].first[1][:sections_attributes].map { |section| Section.find(section[1][:id]) }
+    lessons = params[:chapters].first[1][:lessons_attributes].map { |lesson| Lesson.find(lesson[1][:id]) }
+    lesson_sections = LessonSection.where(section_id: sections.map(&:id), lesson_id: lessons.map(&:id))
+    params[:chapters].first[1][:lessons_attributes].each do |lesson|
+      lesson_section = lesson_sections.find_by(lesson_id: lesson[1][:id])
+      lesson_section.update(number: lesson[1][:number])
+    end
     redirect_to table_of_contents_path
     flash[:notice] = "Order updated."
   end
