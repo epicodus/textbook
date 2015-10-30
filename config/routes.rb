3ee47dd4636f19028ceef2d7ce28766fd6abe488
Christopher Joy-Webb <chris@joywebb.com>
2015-10-30 16:10:39 -0700

    move update_multiple ChaptersController action functionality to update action; clean up views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,7 @@ Textbook::Application.routes.draw do
     resources :lessons, except: [:new, :create, :show]
   end
   resources :lessons, only: [:new, :create, :index]
-  resources :chapters do
-    collection do
-      put :update_multiple, :path => ''
-    end
-  end
+  resources :chapters
   get '/:id', to: 'sections#show', as: :section_show
   get '/:section_id/:id', to: 'lessons#show', as: :lesson_show
 end
