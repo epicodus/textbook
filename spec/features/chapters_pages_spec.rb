c4d5b8fc4ac2ffb6db03bfcf47d6b356c28c8b5f
Christopher Joy-Webb <chris@joywebb.com>
2015-10-30 13:27:54 -0700

    DRY up Chapter integration tests

--- spec/features/chapters_pages_spec.rb
+++ spec/features/chapters_pages_spec.rb
@@ -1,20 +1,22 @@
 require 'spec_helper'
 
-describe Chapter, :js => true do
+describe Chapter, js: true do
+  let(:author) { FactoryGirl.create(:author) }
+  let(:student) { FactoryGirl.create(:student) }
+  let!(:chapter) { FactoryGirl.create(:chapter) }
+
   it 'can be created by an author' do
-    author = FactoryGirl.create :author
-    login_as(author, :scope => :user)
+    login_as(author, scope: :user)
     visit table_of_contents_path
     click_link 'New chapter'
-    fill_in 'Name', :with => 'Awesome chapter'
-    fill_in 'Chapter number', :with => '1'
+    fill_in 'Name', with: 'Awesome chapter'
+    fill_in 'Chapter number', with: '1'
     click_button 'Create Chapter'
     page.should have_content 'Awesome chapter'
   end
 
   it 'displays errors if you try to save an invalid chapter' do
-    author = FactoryGirl.create :author
-    login_as(author, :scope => :user)
+    login_as(author, scope: :user)
     visit table_of_contents_path
     click_link 'New chapter'
     click_button 'Create Chapter'
@@ -22,8 +24,7 @@ describe Chapter, :js => true do
   end
 
   it 'cannot be created by a student' do
-    student = FactoryGirl.create :student
-    login_as(student, :scope => :user)
+    login_as(student, scope: :user)
     visit table_of_contents_path
     page.should_not have_content 'New chapter'
     visit new_chapter_path
@@ -31,9 +32,7 @@ describe Chapter, :js => true do
   end
 
   it 'can be edited by an author' do
-    author = FactoryGirl.create :author
-    login_as(author, :scope => :user)
-    chapter = FactoryGirl.create :chapter
+    login_as(author, scope: :user)
     visit edit_chapter_path(chapter)
     fill_in 'Name', with: 'New awesome chapter'
     click_button 'Update Chapter'
@@ -41,9 +40,7 @@ describe Chapter, :js => true do
   end
 
   it 'cannot be edited by a student' do
-    student = FactoryGirl.create :student
-    login_as(student, :scope => :user)
-    chapter = FactoryGirl.create :chapter
+    login_as(student, scope: :user)
     visit table_of_contents_path
     page.should_not have_content 'edit'
     visit edit_chapter_path chapter
@@ -51,18 +48,14 @@ describe Chapter, :js => true do
   end
 
   it 'can be deleted by an author' do
-    author = FactoryGirl.create :author
-    login_as(author, :scope => :user)
-    chapter = FactoryGirl.create :chapter
+    login_as(author, scope: :user)
     visit table_of_contents_path
     click_link "delete_chapter_#{chapter.id}"
     page.should_not have_content chapter.name
   end
 
   it 'cannot be deleted by a student' do
-    student = FactoryGirl.create :student
-    login_as(student, :scope => :user)
-    chapter = FactoryGirl.create :chapter
+    login_as(student, scope: :user)
     visit table_of_contents_path
     page.should_not have_content 'delete'
   end
