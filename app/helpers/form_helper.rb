563ae3d47f7e5eb5c22f6619f58518a125d9f202
Christopher Joy-Webb <chris@joywebb.com>
2015-11-03 12:18:46 -0800

    DRY up Lesson form partials; add helper method for Lesson form

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,9 +1,17 @@
 module FormHelper
-  def form_for_object(object_type, &block)
+  def ajax_form_for(object_type, &block)
     if params[:action] == 'edit'
       form_for object_type, &block
     elsif params[:action] == 'new'
       form_for object_type, remote: true, &block
+    end
+  end
+
+  def lesson_form(&block)
+    if params[:action] == 'edit'
+      form_for [@section, @lesson], &block
+    elsif params[:action] == 'new'
+      form_for @lesson, &block
     end
   end
 end
