a895ad4e0fbbf3fee6a3b5d1fddf2f2df0d8ebf4
Christopher Joy-Webb <chris@joywebb.com>
2015-11-03 10:20:52 -0800

    update LessonSection model test variables to persist

--- spec/models/lesson_section_spec.rb
+++ spec/models/lesson_section_spec.rb
@@ -6,11 +6,11 @@ describe LessonSection do
   it { should validate_presence_of :lesson }
 
   describe 'default scope' do
-    let(:section) { FactoryGirl.create(:section) }
-    let(:last_lesson) { FactoryGirl.create :lesson, number: 4, section: section }
-    let(:third_lesson) { FactoryGirl.create :lesson, number: 3, section: section }
-    let(:second_lesson) { FactoryGirl.create :lesson, number: 2, section: section }
-    let(:first_lesson) { FactoryGirl.create :lesson, number: 1, section: section }
+    let!(:section) { FactoryGirl.create(:section) }
+    let!(:last_lesson) { FactoryGirl.create :lesson, number: 4, section: section }
+    let!(:third_lesson) { FactoryGirl.create :lesson, number: 3, section: section }
+    let!(:second_lesson) { FactoryGirl.create :lesson, number: 2, section: section }
+    let!(:first_lesson) { FactoryGirl.create :lesson, number: 1, section: section }
     let!(:last_lesson_section) { LessonSection.find_by(lesson_id: last_lesson.id) }
     let!(:first_lesson_section) { LessonSection.find_by(lesson_id: first_lesson.id) }
 
