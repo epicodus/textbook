6321d4ab54cd962d6c25240d2690898fb9befc30
Christopher Joy-Webb <chris@joywebb.com>
2015-11-04 10:11:35 -0800

    update FormHelper methods

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,16 +1,16 @@
 module FormHelper
-  def ajax_form_for(object_type, &block)
-    if params[:action] == 'edit'
+  def chapter_and_section_form(object_type, &block)
+    if params[:action] == 'edit' || params[:action] == 'update'
       form_for object_type, &block
-    elsif params[:action] == 'new'
+    elsif params[:action] == 'new' || params[:action] == 'create'
       form_for object_type, remote: true, &block
     end
   end
 
   def lesson_form(&block)
-    if params[:action] == 'edit'
+    if params[:action] == 'edit' || params[:action] == 'update'
       form_for [@section, @lesson], &block
-    elsif params[:action] == 'new'
+    elsif params[:action] == 'new' || params[:action] == 'create'
       form_for @lesson, &block
     end
   end
