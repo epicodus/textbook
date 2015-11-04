ed0f29fb661c12af74d5dc340d064e73c65f20e0
Christopher Joy-Webb <chris@joywebb.com>
2015-11-04 10:25:08 -0800

    add LessonSection before_validation method that sets number
    This moves the setting of number from Lesson to LessonSection; tests are updated as well

--- app/models/lesson_section.rb
+++ app/models/lesson_section.rb
@@ -2,8 +2,17 @@ class LessonSection < ActiveRecord::Base
   belongs_to :lesson
   belongs_to :section
 
+  before_validation :set_number, on: :create
+
   validates :section, presence: true
   validates :lesson, presence: true
+  validates :number, presence: true, numericality: { only_integer: true }
 
   default_scope -> { order :number }
+
+private
+
+  def set_number
+    self.number = section.try(:lesson_sections).try(:last).try(:number).to_i + 1
+  end
 end
--- spec/models/lesson_section_spec.rb
+++ spec/models/lesson_section_spec.rb
@@ -3,14 +3,35 @@ require 'spec_helper'
 describe LessonSection do
 
   it { should validate_presence_of :section }
-  it { should validate_presence_of :lesson }
+
+  describe 'validations' do
+    it 'validates that a lesson section is created with a lesson' do
+      section = FactoryGirl.create(:section)
+      lesson_section = LessonSection.new(section_id: section.id, lesson_id: nil)
+      expect(lesson_section.valid?).to be false
+    end
+
+    it 'validates that a lesson section always has a number' do
+      section = FactoryGirl.create(:section)
+      lesson = FactoryGirl.create(:lesson)
+      lesson_section = LessonSection.create(section_id: section.id, lesson_id: lesson.id)
+      expect(lesson_section.update(number: nil)).to be false
+    end
+
+    it 'validates that a lesson section always has a number that is an integer' do
+      section = FactoryGirl.create(:section)
+      lesson = FactoryGirl.create(:lesson)
+      lesson_section = LessonSection.create(section_id: section.id, lesson_id: lesson.id)
+      expect(lesson_section.update(number: 1.7)).to be false
+    end
+  end
 
   describe 'default scope' do
     let!(:section) { FactoryGirl.create(:section) }
-    let!(:last_lesson) { FactoryGirl.create :lesson, number: 4, section: section }
-    let!(:third_lesson) { FactoryGirl.create :lesson, number: 3, section: section }
-    let!(:second_lesson) { FactoryGirl.create :lesson, number: 2, section: section }
-    let!(:first_lesson) { FactoryGirl.create :lesson, number: 1, section: section }
+    let!(:first_lesson) { FactoryGirl.create :lesson, section: section }
+    let!(:second_lesson) { FactoryGirl.create :lesson, section: section }
+    let!(:third_lesson) { FactoryGirl.create :lesson, section: section }
+    let!(:last_lesson) { FactoryGirl.create :lesson, section: section }
     let!(:last_lesson_section) { LessonSection.find_by(lesson_id: last_lesson.id) }
     let!(:first_lesson_section) { LessonSection.find_by(lesson_id: first_lesson.id) }
 
