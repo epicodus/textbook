c05f5f48488b97796d22c8d03983a8fe4ca32204
Christopher Joy-Webb <chris@joywebb.com>
2015-11-11 13:49:32 -0800

    update routes to nest sections in courses for certain routes
    This nests sections within courses for :new, :create, :edit:, :update routes because New section button is removed from main page

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -8,8 +8,27 @@ class SectionsController < InheritedResources::Base
     redirect_to courses_path
   end
 
+  def new
+    @course = Course.find(params[:course_id])
+  end
+
+  def create
+    course = Course.find(params[:course_id])
+    section = Section.new(section_params)
+    if section.save
+      redirect_to course_path(course), notice: 'Section saved.'
+    else
+      @course = Course.find(params[:course_id])
+      render 'new'
+    end
+  end
+
   def show
     @section = Section.find(params[:id])
+  end
+
+  def edit
+    @course = Course.find(params[:course_id])
   end
 
   def update
@@ -17,6 +36,7 @@ class SectionsController < InheritedResources::Base
     if section.update(section_params)
       redirect_to section_show_path(section), notice: "Section updated."
     else
+      @course = Course.find(params[:course_id])
       render 'edit'
     end
   end
