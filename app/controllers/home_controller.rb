279776f23ab465c7b3788195428a800d81732358
Christopher Joy-Webb <chris@joywebb.com>
2015-11-17 15:55:38 -0800

    update HomeController to scope front page less to first Course

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def show
     if Lesson.accessible_by(current_ability).any?
-      redirect_to section_lesson_show_path(Section.first, Section.first.lessons.accessible_by(current_ability).first)
+      first_section = Course.accessible_by(current_ability).first.sections.accessible_by(current_ability).first
+      redirect_to section_lesson_show_path(first_section, first_section.lessons.accessible_by(current_ability).first)
     end
   end
 end
