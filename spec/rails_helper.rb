0eb5f39061c797f9fb8f1b8cd837da4fcf33ceed
Christopher Joy-Webb <chris@joywebb.com>
2015-12-15 10:35:26 -0800

    update rspec configuration + add rails_helper.rb file

---
+++ spec/rails_helper.rb
@@ -0,0 +1,43 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'capybara/poltergeist'
+require 'simplecov'
+require 'coveralls'
+
+include Warden::Test::Helpers
+Warden.test_mode!
+
+Capybara.default_driver = :rack_test
+Capybara.javascript_driver = :poltergeist
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  config.order = 'random'
+  config.before(:each) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter,
+]
+SimpleCov.start
