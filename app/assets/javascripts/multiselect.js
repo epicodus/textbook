d0c1db3a9666e633d2af91e35dcbc0386df695e4
Christopher Joy-Webb <chris@joywebb.com>
2015-12-18 13:54:06 -0800

    update views and scripts to accomodate Lesson day of week and deleting LessonSections

--- app/assets/javascripts/multiselect.js
+++ app/assets/javascripts/multiselect.js
@@ -4,11 +4,12 @@ $(function () {
     maxHeight: 500,
     onChange: function(option) {
       $('#section-' + option.val() + '-lesson-type').toggle();
+      $('#section-' + option.val() + '-lesson-type').find('input[type=radio]:checked').attr('checked', false);
     }
   });
 
   $('.lesson-type').each(function() {
-    if ($(this).find('input[type=radio]:checked').length === 1) {
+    if ($(this).find('input[type=radio]:checked').length === 2) {
       $(this).show();
     } else {
       $(this).hide();
