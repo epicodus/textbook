9703396630b75f9f13c3e2ac205848c957a06ee6
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2015-12-30 12:04:44 -0800

    use friendlier urls

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,15 @@ Textbook::Application.routes.draw do
   get 'table-of-contents', to: redirect('courses')
 
   resources :lessons
-  resources :courses do
-    resources :sections do
+  resources :courses, only: [:index, :new, :create] do
+    put :update_multiple, on: :collection
+  end
+  resources :courses, only: [:edit, :update, :destroy], path: '/'
+  resources :courses, only: [:show], path: '/' do
+    resources :sections, only: [:index, :new, :create]
+    resources :sections, only: [:show, :edit, :update, :destroy], path: '/' do
       resources :lessons, only: [:index]
     end
-    put :update_multiple, on: :collection
   end
 
   get '/:course_id/:section_id/:id', to: 'lessons#show', as: :course_section_lesson_show
