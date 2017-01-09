2a37a769e9e99a37ff23b228c0e9e7f90f15be17
Mike Goren <mike@mortalwombat.net>
2017-01-09 12:59:04 -0800

    update gems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui/sortable
+//= require jquery-ui/widgets/sortable
 //= require bootstrap-sprockets
 //= require bootstrap-multiselect
 //= require_tree .
