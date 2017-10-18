b197fb7dd809ed51bbe26b8bda0747f5d93a0ff3
Mike Goren <mike@mortalwombat.net>
2017-10-17 23:37:23 -0700

    update gems

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,86 +15,81 @@ ActiveRecord::Schema.define(version: 20170824182837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "courses", force: :cascade do |t|
-    t.string   "name",       limit: 255,                 null: false
-    t.integer  "number",                                 null: false
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.string   "slug",       limit: 255,                 null: false
-    t.boolean  "public",                 default: false, null: false
+  create_table "courses", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255, null: false
+    t.integer "number", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "slug", limit: 255, null: false
+    t.boolean "public", default: false, null: false
     t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_courses_on_deleted_at"
+    t.index ["name"], name: "index_courses_on_name", unique: true
   end
 
-  add_index "courses", ["deleted_at"], name: "index_courses_on_deleted_at", using: :btree
-  add_index "courses", ["name"], name: "index_courses_on_name", unique: true, using: :btree
-
-  create_table "lesson_sections", force: :cascade do |t|
-    t.integer  "lesson_id"
-    t.integer  "section_id"
-    t.integer  "number"
+  create_table "lesson_sections", id: :serial, force: :cascade do |t|
+    t.integer "lesson_id"
+    t.integer "section_id"
+    t.integer "number"
     t.datetime "deleted_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "work_type",   default: 0
-    t.integer  "day_of_week", default: 0
+    t.integer "work_type", default: 0
+    t.integer "day_of_week", default: 0
+    t.index ["lesson_id"], name: "index_lesson_sections_on_lesson_id"
+    t.index ["number"], name: "index_lesson_sections_on_number"
+    t.index ["section_id"], name: "index_lesson_sections_on_section_id"
   end
 
-  add_index "lesson_sections", ["lesson_id"], name: "index_lesson_sections_on_lesson_id", using: :btree
-  add_index "lesson_sections", ["number"], name: "index_lesson_sections_on_number", using: :btree
-  add_index "lesson_sections", ["section_id"], name: "index_lesson_sections_on_section_id", using: :btree
-
-  create_table "lessons", force: :cascade do |t|
-    t.string   "name",           limit: 255,                 null: false
-    t.text     "content",                                    null: false
-    t.integer  "old_section_id"
-    t.integer  "old_number"
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
+  create_table "lessons", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255, null: false
+    t.text "content", null: false
+    t.integer "old_section_id"
+    t.integer "old_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.datetime "deleted_at"
-    t.string   "slug",           limit: 255
-    t.boolean  "public",                     default: false, null: false
-    t.string   "video_id",       limit: 255
-    t.text     "cheat_sheet"
-    t.text     "update_warning"
-    t.text     "markdown"
-    t.boolean  "old_tutorial"
-    t.text     "teacher_notes"
+    t.string "slug", limit: 255
+    t.boolean "public", default: false, null: false
+    t.string "video_id", limit: 255
+    t.text "cheat_sheet"
+    t.text "update_warning"
+    t.text "markdown"
+    t.boolean "old_tutorial"
+    t.text "teacher_notes"
+    t.index ["name"], name: "index_lessons_on_name"
   end
 
-  add_index "lessons", ["name"], name: "index_lessons_on_name", using: :btree
-
-  create_table "sections", force: :cascade do |t|
-    t.string   "name",       limit: 255,                 null: false
-    t.integer  "number",                                 null: false
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.integer  "course_id",                              null: false
-    t.string   "slug",       limit: 255,                 null: false
-    t.boolean  "public",                 default: false, null: false
-    t.integer  "week"
+  create_table "sections", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255, null: false
+    t.integer "number", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "course_id", null: false
+    t.string "slug", limit: 255, null: false
+    t.boolean "public", default: false, null: false
+    t.integer "week"
     t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_sections_on_deleted_at"
+    t.index ["name"], name: "index_sections_on_name"
   end
 
-  add_index "sections", ["deleted_at"], name: "index_sections_on_deleted_at", using: :btree
-  add_index "sections", ["name"], name: "index_sections_on_name", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "",    null: false
-    t.string   "encrypted_password",     limit: 255, default: "",    null: false
-    t.string   "reset_password_token",   limit: 255
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", limit: 255, default: "", null: false
+    t.string "encrypted_password", limit: 255, default: "", null: false
+    t.string "reset_password_token", limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer "sign_in_count", default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                         null: false
-    t.datetime "updated_at",                                         null: false
-    t.boolean  "author",                             default: false
+    t.string "current_sign_in_ip", limit: 255
+    t.string "last_sign_in_ip", limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "author", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
