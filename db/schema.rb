b3e8ee427d5982b926595a6c5ea60ad1f6351839
Mike Goren <mike@mortalwombat.net>
2018-03-26 16:07:31 -0700

    allow courses without lesson dates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180201235206) do
+ActiveRecord::Schema.define(version: 20180326225342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180201235206) do
     t.string "slug", limit: 255, null: false
     t.boolean "public", default: false, null: false
     t.datetime "deleted_at"
+    t.boolean "dateless"
     t.index ["deleted_at"], name: "index_courses_on_deleted_at"
     t.index ["name"], name: "index_courses_on_name", unique: true
   end
