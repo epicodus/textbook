a0055c3e7351afef520c77b360d75feb197288f3
Mike Goren <mike@mortalwombat.net>
2019-09-18 16:50:22 -0700

    update format of chrome headless options

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,7 +22,7 @@ end
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w(headless disable-gpu) }
+    "goog:chromeOptions": { args: ['headless'] }
   )
 
   Capybara::Selenium::Driver.new app,
