4eed664f253bee852ea7b67465ea87552c310f71
Mike Goren <mike@mortalwombat.net>
2021-03-02 13:58:13 -0800

    add id tags to headers for TOC linking

--- app/helpers/lesson_helper.rb
+++ app/helpers/lesson_helper.rb
@@ -1,6 +1,6 @@
 module LessonHelper
   def markdown(text)
-    html = Redcarpet::Render::HTML.new(:prettify => true)
+    html = Redcarpet::Render::HTML.new(:prettify => true, :with_toc_data => true)
     markdown = Redcarpet::Markdown.new(html, :space_after_headers => true, :fenced_code_blocks => true, :tables => true)
     markdown.render(text).html_safe
   end
