abaeb50ed7b5f05a0218d78d412e58c5f077c011
Mike Goren <mike@mortalwombat.net>
2022-05-22 16:51:00 -0700

    update rails to 6.1

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,6 @@
 if Rails.env == 'production'
   uri = URI.parse(ENV["REDISTOGO_URL"])
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  Resque.redis = Redis.new(:host => 'localhost', :port => '6379', :password => ENV['REDIS_PASSWORD'])
 end
