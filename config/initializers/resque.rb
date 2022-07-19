a76b115168748db1f2fe8bc39f24ebde42872cb3
Mike Goren <mike@mortalwombat.net>
2022-07-19 13:28:32 -0700

    migrate to heroku-redis

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  uri = URI.parse(ENV["REDISTOGO_URL"])
+  uri = URI.parse(ENV["REDIS_URL"])
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
   Resque.redis = Redis.new(:host => 'localhost', :port => '6379', :password => ENV['REDIS_PASSWORD'])
