9b9a8edcb4034955c80ca9ab0719ac8840881bef
Mike Goren <mike@mortalwombat.net>
2022-11-03 00:21:14 -0700

    open all links in markdown in new tab

--- app/helpers/lesson_helper.rb
+++ app/helpers/lesson_helper.rb
@@ -1,6 +1,6 @@
 module LessonHelper
   def markdown(text)
-    html = Redcarpet::Render::HTML.new(:prettify => true, :with_toc_data => true)
+    html = Redcarpet::Render::HTML.new(:prettify => true, :with_toc_data => true, :link_attributes => { target: "_blank", rel:"noopener noreferrer" })
     markdown = Redcarpet::Markdown.new(html, :space_after_headers => true, :fenced_code_blocks => true, :tables => true)
     markdown.render(text).html_safe
   end
