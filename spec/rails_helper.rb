7edfe204ef6c85df9d3ca30101c2ecb7dc615267
Mike Goren <mike@epicodus.com>
2023-09-17 15:59:44 -0700

    update gems & selenium usage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,13 +40,26 @@ Warden.test_mode!
 
 WebMock.enable!
 
+# Capybara.register_driver :selenium_chrome_headless do |app|
+#   options = ::Selenium::WebDriver::Chrome::Options.new
+#   options.add_argument('--headless')
+#   options.add_argument('--no-sandbox')
+#   options.add_argument('--no-gpu')
+#   options.add_argument('--remote-debugging-port=9222')
+#   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+# end
+# Capybara.javascript_driver = :selenium_chrome_headless
+
+capabilities = Selenium::WebDriver::Chrome::Options.new(
+  args: %w[--headless --no-sandbox --disable-gpu --remote-debugging-port=9222],
+  binary: ENV.fetch('GOOGLE_CHROME_SHIM', nil),
+)
 Capybara.register_driver :selenium_chrome_headless do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--no-gpu')
-  options.add_argument('--remote-debugging-port=9222')
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    options: capabilities
+  )
 end
 Capybara.javascript_driver = :selenium_chrome_headless
 
